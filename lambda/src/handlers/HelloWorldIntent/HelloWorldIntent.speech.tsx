e38dd6986bb125dd094df0c07cf290098fee6ad5
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-29 20:06:38 +0900

    add example for props

--- lambda/src/handlers/HelloWorldIntent/HelloWorldIntent.speech.tsx
+++ lambda/src/handlers/HelloWorldIntent/HelloWorldIntent.speech.tsx
@@ -7,6 +7,18 @@ import { IntentRequest } from 'ask-sdk-model'
 
 export class HelloWorldIntentScript extends SpeechScriptJSX<IntentRequest> {
     speech() {
+        const isNewSession = this.props.session.new
+        const {
+            locale
+        } = this.props.request
+        if (isNewSession) {
+            return (
+                <speak>
+                    <p>はじめまして！</p>
+                    <p>{locale}から起動しました。</p>
+                </speak>
+            )
+        }
         return (
             <speak>
                 <p>こんにちは！</p>
